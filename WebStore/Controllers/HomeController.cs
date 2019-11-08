f694788b6ae954f8441c1f38a68efae2e061a87e
Павел <shmachilin@yandex.ru>
2019-11-08 21:14:05 +0300

    Работа с представлениями

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -16,5 +17,20 @@ namespace WebStore.Controllers
         public IActionResult Index() => View();
 
         public IActionResult ReadConfig() => Content(_Configuration["CustomData"]);
+
+        private static readonly List<EmployeeView> __Employees = new List<EmployeeView>
+        {
+            new EmployeeView { Id = 1, SecondName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 35 },
+            new EmployeeView { Id = 1, SecondName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 25 },
+            new EmployeeView { Id = 1, SecondName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
+        public IActionResult GetEmployes()
+        {
+            ViewBag.SomeData = "Hello World!";
+            ViewData["Test"] = "TestData";
+
+            return View(__Employees);
+        }
     }
 }
