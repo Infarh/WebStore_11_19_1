a8c35db77bb9e76e0b9bd2eee4531bb989bbd8ec
Павел <shmachilin@yandex.ru>
2019-11-08 20:37:26 +0300

    Первый контроллер

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _Configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return Content(_Configuration["CustomData"]);
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -14,7 +14,7 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -24,9 +24,17 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync(Configuration["CustomData"]);
+            //});
+
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync(Configuration["CustomData"]);
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
