75b3435475248b30e8ebb6e3c19dac29640c21d4
Павел <shmachilin@yandex.ru>
2019-11-12 21:38:16 +0300

    Пример работы с сотрудниками

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
 using WebStore.ViewModels;
 
 namespace WebStore.Controllers
@@ -22,6 +23,43 @@ namespace WebStore.Controllers
             ViewData["Test"] = "TestData";
 
             return View(__Employees);
+        }
+
+        public IActionResult Details(int? Id)
+        {
+            if (Id is null)
+                return BadRequest();
+
+            var employee = __Employees.FirstOrDefault(e => e.Id == Id);
+            if (employee is null)
+                return NotFound();
+
+            return View(employee);
+        }
+
+        public IActionResult DetailsName(string FirstName, string LastName)
+        {
+            if (FirstName is null && LastName is null)
+                return BadRequest();
+
+            IEnumerable<EmployeeView> employees = __Employees;
+            if (!string.IsNullOrWhiteSpace(FirstName))
+                employees = employees.Where(e => e.FirstName == FirstName);
+            if (!string.IsNullOrWhiteSpace(LastName))
+                employees = employees.Where(e => e.SecondName == LastName);
+
+            var employee = employees.FirstOrDefault();
+
+            if (employee is null)
+                return NotFound();
+
+            return View(nameof(Details), employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [FromBody] EmployeeView Employee)
+        {
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- WebStore/ViewModels/EmployeeView.cs
+++ WebStore/ViewModels/EmployeeView.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.ViewModels
 {
     public class EmployeeView
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
 
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Фамилия")]
         public string SecondName { get; set; }
 
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
 
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
     }
 }
