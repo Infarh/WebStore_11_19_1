dccff4af129bcc2d7c7d07ba23a3d457cef3ae26
Павел <shmachilin@yandex.ru>
2019-11-12 21:16:12 +0300

    Подключение BrowserLink

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -22,6 +22,7 @@ namespace WebStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
 
             app.UseStaticFiles();
