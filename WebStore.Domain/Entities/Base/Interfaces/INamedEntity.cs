f0e36aeb5edcab7feb89dc3fa03e816b5b794389
Павел <shmachilin@yandex.ru>
2019-11-19 20:20:44 +0300

    Базовые интерфейсы сущностей

---
+++ WebStore.Domain/Entities/Base/Interfaces/INamedEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStore.Domain.Entities.Base.Interfaces
+{
+    /// <summary>Именованная сущность</summary>
+    public interface INamedEntity : IBaseEntity
+    {
+        /// <summary>Имя</summary>
+        string Name { get; set; }
+    }
+}
