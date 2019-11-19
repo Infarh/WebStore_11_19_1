36537720f0c5dbdbd414b1f2715498fc7dbc4aa0
Павел <shmachilin@yandex.ru>
2019-11-19 20:25:19 +0300

    Первые сущности

---
+++ WebStore.Domain/Entities/Section.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
