14a889409b4147c775b312aa8e7489a20f0b7f51
Павел <shmachilin@yandex.ru>
2019-11-19 20:22:39 +0300

    Базовые сущности

---
+++ WebStore.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities.Base
+{
+    /// <summary>Сущность</summary>
+    public abstract class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
