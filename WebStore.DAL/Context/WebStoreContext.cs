b3c10068aa2f9dc50366d4ee61680766b5e5dcb4
Pavel <shmachilin@gmail.com>
2019-11-23 17:23:07 +0300

    Контекст БД + строка подключения

---
+++ WebStore.DAL/Context/WebStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public WebStoreContext(DbContextOptions<WebStoreContext> options) : base(options) { }
+
+        //protected override void OnModelCreating(ModelBuilder model)
+        //{
+        //    base.OnModelCreating(model);
+        //}
+    }
+}
