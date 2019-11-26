18033f301305239956ca4c6df314aaf4dbf5598c
Pavel <shmachilin@gmail.com>
2019-11-26 20:55:21 +0300

    Визуальный компонент представления пользвоателя в заголовке страницы

---
+++ WebStore/Components/UserInfoViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class UserInfoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => User.Identity.IsAuthenticated
+            ? View("UserInfoView")
+            : View();
+    }
+}
