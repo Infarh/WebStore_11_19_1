26c1040aa4737b0c2a460b23da464e51b1a93ddd
Infarh <shmachilin@gmail.com>
2019-11-30 18:55:21 +0300

    Пример автоматической генерации кода

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -91,6 +91,10 @@ namespace WebStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
