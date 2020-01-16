1012180a76d263c4e165bfff5717b89154069719
Infarh <shmachilin@yandex.ru>
2020-01-16 19:36:03 +0300

    Регистрация сервисов на стороне ServiceHosting

--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebStore.DAL.Context;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Database;
+using WebStore.Services.Product;
 
 namespace WebStore.ServiceHosting
 {
@@ -14,6 +20,17 @@ namespace WebStore.ServiceHosting
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<WebStoreContextInitializer>();
+
+            services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
+            services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<IOrderService, SqlOrderService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<ICartService, CookieCartService>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
