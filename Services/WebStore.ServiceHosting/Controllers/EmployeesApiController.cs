6033c02ff8e01370d5079e3d7e63df7ba80eee2d
Infarh <shmachilin@yandex.ru>
2020-01-16 19:44:26 +0300

    Контроллер WebAPI для IEmployeesData

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/employees")]
+    //[Route("api/[controller]")] // путь: api/EmployeesApi
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IEmployeesData
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+
+        [HttpGet, ActionName("Get")] // api/Employees  // api/Employees/Get
+        public IEnumerable<EmployeeView> GetAll() => _EmployeesData.GetAll();
+
+        [HttpGet("{id}"), ActionName("Get")]
+        public EmployeeView GetById(int id) => _EmployeesData.GetById(id);
+
+        [HttpPost, ActionName("Post")]
+        public void Add(EmployeeView Employee) => _EmployeesData.Add(Employee);
+
+        [HttpPut("{id}"), ActionName("Put")]
+        public void Edit(int id, EmployeeView Employee) => _EmployeesData.Edit(id, Employee);
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id) => _EmployeesData.Delete(id);
+
+        [NonAction]
+        public void SaveChanges() => _EmployeesData.SaveChanges();
+    }
+}
