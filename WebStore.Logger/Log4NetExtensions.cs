2486863c75c214061e2fed1ec585fecbb891d976
Infarh <shmachilin@yandex.ru>
2020-01-23 20:56:14 +0300

    Расширения системы логирования для использования в ней библиотеки Log4NET

---
+++ WebStore.Logger/Log4NetExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetExtensions 
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory Factory, string ConfigurationFile = "log4net.config")
+        {
+            if (!Path.IsPathRooted(ConfigurationFile))
+            {
+                var assembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Не удалось определить сборку с точкой входа в приложение");
+                var dir = Path.GetDirectoryName(assembly.Location) ?? throw new InvalidOperationException("Не удалось определить каталог исполнительного файла");
+                ConfigurationFile = Path.Combine(dir, ConfigurationFile);
+            }
+
+            Factory.AddProvider(new Log4NetProvider(ConfigurationFile));
+
+            return Factory;
+        }
+    }
+}
---
+++ WebStore.Logger/Log4NetLogger.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using Microsoft.Extensions.Logging;
+using log4net;
+
+namespace WebStore.Logger
+{
+    public class Log4NetLogger : ILogger
+    {
+        private readonly ILog _Log;
+
+        public Log4NetLogger(string CategoryName, XmlElement Configuration)
+        {
+            var logger_repository = LogManager.CreateRepository(
+                Assembly.GetEntryAssembly(), 
+                typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+            _Log = LogManager.GetLogger(logger_repository.Name, CategoryName);
+            log4net.Config.XmlConfigurator.Configure(logger_repository, Configuration);
+        }
+
+        public bool IsEnabled(LogLevel Level)
+        {
+            switch (Level)
+            {
+                default: throw new ArgumentOutOfRangeException(nameof(Level), Level, null);
+
+                case LogLevel.Trace: 
+                case LogLevel.Debug:
+                    return _Log.IsDebugEnabled;
+
+                case LogLevel.Information:
+                    return _Log.IsInfoEnabled;
+
+                case LogLevel.Warning:
+                    return _Log.IsWarnEnabled;
+
+                case LogLevel.Error:
+                    return _Log.IsErrorEnabled;
+
+                case LogLevel.Critical:
+                    return _Log.IsFatalEnabled;
+
+                case LogLevel.None:
+                    return false;
+            }
+        }
+
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public void Log<TState>(LogLevel Level, EventId EventId, TState State, Exception Error, Func<TState, Exception, string> Formatter)
+        {
+            if(Formatter is null) throw new ArgumentNullException(nameof(Formatter));
+
+            if(!IsEnabled(Level)) return;
+
+            var log_message = Formatter(State, Error);
+
+            if (string.IsNullOrEmpty(log_message) && Error is null) return;
+
+            switch (Level)
+            {
+                default: throw new ArgumentOutOfRangeException(nameof(Level), Level, null);
+
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                    _Log.Debug(log_message);
+                    break;
+
+                case LogLevel.Information: 
+                    _Log.Info(log_message);
+                    break;
+
+                case LogLevel.Warning: 
+                    _Log.Warn(log_message);
+                    break;
+
+                case LogLevel.Error: 
+                    _Log.Error(log_message ?? Error.ToString());
+                    break;
+
+                case LogLevel.Critical: 
+                    _Log.Fatal(log_message ?? Error.ToString());
+                    break;
+
+                case LogLevel.None: 
+                    break;
+            }
+
+        }
+    }
+}
