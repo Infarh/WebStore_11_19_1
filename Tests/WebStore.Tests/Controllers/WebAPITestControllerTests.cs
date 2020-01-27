7194b644bc3aee0fb592481eea3f2e20c0b9b750
Infarh <shmachilin@yandex.ru>
2020-01-27 20:41:55 +0300

    Тестирование контроллера WebAPITestController

---
+++ Tests/WebStore.Tests/Controllers/WebAPITestControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Controllers;
+using WebStore.Interfaces.Api;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class WebAPITestControllerTests
+    {
+        [TestMethod]
+        public async Task Index_Returns_View_With_Values()
+        {
+            var expected_values = new[] { "1", "2", "3" };
+
+            var value_service_mock = new Mock<IValuesService>();
+            value_service_mock
+               .Setup(service => service.GetAsync())
+               .ReturnsAsync(expected_values);
+
+            var controller = new WebAPITestController(value_service_mock.Object);
+
+            var result = await controller.Index();
+
+            var view_result = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<string>>(view_result.Model);
+
+            Assert.Equal(expected_values.Length, model.Count());
+
+            value_service_mock.Verify(service => service.GetAsync());
+            value_service_mock.VerifyNoOtherCalls();
+        }
+    }
+}
