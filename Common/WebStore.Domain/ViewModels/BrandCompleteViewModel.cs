f44f6ce6c4da7382b41b66a23823a4b3d893d2fb
Infarh <shmachilin@yandex.ru>
2020-01-30 20:22:21 +0300

    Подсветка выбранного бренда в каталоге товаров

---
+++ Common/WebStore.Domain/ViewModels/BrandCompleteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class BrandCompleteViewModel
+    {
+        public IEnumerable<BrandViewModel> Brands { get; set; }
+
+        public int? CurrentBrandId { get; set; }
+    }
+}
