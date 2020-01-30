fdb48d127479da7be7e3ead2fb9a5c57d5684083
Infarh <shmachilin@yandex.ru>
2020-01-30 20:44:15 +0300

    Представление компонента "хлебных крошек"

---
+++ Common/WebStore.Domain/ViewModels/BreadCrumbs/BreadCrumbsType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.ViewModels.BreadCrumbs
+{
+    public enum BreadCrumbsType : byte
+    {
+        None,
+        Section,
+        Brand,
+        Product
+    }
+}
