de6d100276f4ea0abe96103bede00c3e728384e6
Infarh <shmachilin@yandex.ru>
2020-01-30 19:49:05 +0300

    Активация активной ссылки в разметке

--- UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
+++ UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
@@ -35,10 +35,50 @@ namespace WebStore.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            //if (IsActive())
-            //    MakeActive(output);
+            if (IsActive())
+                MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
+        }
+
+        private bool IsActive()
+        {
+            var route_values = ViewContext.RouteData.Values;
+            var current_controller = route_values["Controller"].ToString();
+            var current_action = route_values["Action"].ToString();
+
+            const StringComparison ignore_case = StringComparison.OrdinalIgnoreCase;
+
+            if (!string.IsNullOrWhiteSpace(Controller) && !string.Equals(Controller, current_controller, ignore_case))
+                return false;
+
+            if (!string.IsNullOrWhiteSpace(Action) && !string.Equals(Action, current_action, ignore_case))
+                return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key].ToString() != value)
+                    return false;
+
+            return true;
+        }
+
+        private void MakeActive(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(a => a.Name == "class");
+
+            if (class_attribute is null)
+                output.Attributes.Add(new TagHelperAttribute("class", "active"));
+            else
+            {
+                if(class_attribute.Value.ToString().Contains("active")) return;
+
+                output.Attributes.SetAttribute(
+                    "class",
+                    class_attribute.Value is null
+                        ? "active"
+                        : class_attribute.Value + " active"
+                );
+            }
         }
     }
 }
