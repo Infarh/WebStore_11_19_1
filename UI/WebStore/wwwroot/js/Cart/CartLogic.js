2f64dc98b525b8f31595092389c30023a54f2a0e
Infarh <shmachilin@yandex.ru>
2020-02-03 20:12:39 +0300

    Асинхрнное добавление товаров в корзину с помощью JS

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -0,0 +1,47 @@
+﻿Cart = {
+    _properties: {
+        getCartViewLink: "",
+        addToCartLink: ""
+    },
+
+    init: function (properties) {
+        $.extend(Cart._properties, properties);
+
+        Cart.initEvents();
+    },
+
+    initEvents: function () {
+        $(".add-to-cart").click(Cart.addToCart);
+    },
+
+    addToCart: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        var id = button.data("id");
+
+        //$.get(Cart._properties.addToCartLink + "/" + id)
+        $.get(`${Cart._properties.addToCartLink}/${id}`)
+            .done(function () {
+                Cart.showToolTip(button);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("addToCart fail"); });
+    },
+
+    showToolTip: function (button) {
+        button.tooltip({ title: "Добавлено в корзину" }).tooltip("show");
+        setTimeout(function () {
+            button.tooltip("destroy");
+        }, 500);
+    },
+
+    refreshCartView: function () {
+        var container = $("#cart-container");
+        $.get(Cart._properties.getCartViewLink)
+            .done(function(cart_html) {
+                container.html(cart_html);
+            })
+            .fail(function () { console.log("refreshCartView fail"); });
+    }
+}
